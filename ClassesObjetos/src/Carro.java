86526b80e23af287df411e077841589925917e06
Daniel <dfpires@gmail.com>
2026-02-23 08:53:23 -0300

    Classes e Objetos - métodos de instância

--- ClassesObjetos/src/Carro.java
+++ ClassesObjetos/src/Carro.java
@@ -4,19 +4,28 @@ public class Carro{
     public String marca, modelo, cor, placa;
     public float potencia;
     public int ano;
+    public boolean motor; // true se ligado e false se desligado
 
     // método construtor
     public Carro(String marca, String modelo, float potencia, int ano, String placa, String cor){
         this.marca = marca; this.modelo = modelo;
         this.potencia = potencia; this.ano = ano;
         this.placa = placa; this.cor = cor;
+        this.motor = false;
     }
+    // ligar o carro
+    public void ligar(){
+        this.motor = true;
+    }
+
     // converte o objeto em String
     public String toString(){
         // this representa o objeto que chama o método
         return " Marca " + this.marca +
                 " Modelo " + this.modelo +
                 " Placa " + this.placa +
-                " Ano " + this.ano;
+                " Ano " + this.ano +
+                " Motor: " + // operador ternário
+                (this.motor ? "Ligado" : "Desligado") ;
     }
 }
--- ClassesObjetos/src/TestaCarro.java
+++ ClassesObjetos/src/TestaCarro.java
@@ -4,21 +4,14 @@ public class TestaCarro {
         Carro obj1 = new Carro("Fiat", "Palio", 1.6f, 2020, "A", "Azul");
         System.out.println(obj1.toString());
 
-        Carro obj2 = new Carro();
-        obj2.marca = "Fiat";
-        obj2.modelo = "Palio";
-        obj2.potencia = 1.6f; // f força virar float
-        obj2.ano = 2020;
-        obj2.placa = "A";
-        obj2.cor = "Azul";
+        Carro obj2 = new Carro("GM", "Onix", 1.3f, 2021, "B", "Preto");
         System.out.println(obj2.toString());
-        Carro obj3 = new Carro();
-        obj3.marca = "Fiat";
-        obj3.modelo = "Palio";
-        obj3.potencia = 1.6f; // f força virar float
-        obj3.ano = 2020;
-        obj3.placa = "A";
-        obj3.cor = "Azul";
+        obj2.ligar();
+        System.out.println(obj2.toString());
+
+        Carro obj3 = new Carro("Volks", "Gol", 1.0f, 2022, "C", "Branco");
+        System.out.println(obj3.toString());
+        obj3.ligar();
         System.out.println(obj3.toString());
     }
 }
