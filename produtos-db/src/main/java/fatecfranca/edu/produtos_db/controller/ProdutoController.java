086c833c7df54679e1ff3d766be58f19e28e8bfe
Daniel <dfpires@gmail.com>
2026-05-19 12:43:27 -0300

    remove e atualiza

--- produtos-db/src/main/java/fatecfranca/edu/produtos_db/controller/ProdutoController.java
+++ produtos-db/src/main/java/fatecfranca/edu/produtos_db/controller/ProdutoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
@@ -18,16 +19,37 @@ public class ProdutoController {
     }
     @GetMapping
     public ResponseEntity<List<Produto>> listar(){
+
         return ResponseEntity.ok(service.listar());
     }
     @GetMapping("/{id}")
-    public ResponseEntity<Produto> buscar(@PathVariable Long id){
-        return ResponseEntity.ok(service.buscar(id));
+    public ResponseEntity<Optional<Produto>> buscar(@PathVariable Long id){
+        Optional<Produto> result = service.buscar(id);
+        if (result.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
     }
     @PostMapping
     public ResponseEntity<Produto> salvar(@RequestBody Produto produto){
         Produto salvo = service.salvar(produto);
         URI uri = URI.create("/produtos/" + salvo.getId());
         return ResponseEntity.created(uri).body(salvo);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> remover(@PathVariable Long id){
+        if (service.remover(id)){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Produto> atualizar(@PathVariable Long id,
+                                  @RequestBody Produto atual){
+        Produto aux = (service.atualizar(id, atual));
+        if (aux == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(atual);
     }
 }
--- produtos-db/src/main/java/fatecfranca/edu/produtos_db/service/ProdutoService.java
+++ produtos-db/src/main/java/fatecfranca/edu/produtos_db/service/ProdutoService.java
@@ -6,6 +6,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
@@ -20,16 +21,27 @@ public class ProdutoService {
         return repo.findAll();
     }
     // buscar por id
-    public Produto buscar(Long id){
-        return repo.findById(id)
-                .orElseThrow( () -> new RuntimeException("Não encontrado"));
+    public Optional<Produto> buscar(Long id){
+        return repo.findById(id);
     }
     @Transactional
     public Produto salvar(Produto p){
-        return repo.save(p);
+        return repo.save(p); // objeto p sem id
     }
     @Transactional
-    public void remover(Long id){
-        repo.deleteById(id);
+    public boolean remover(Long id){
+        if (repo.existsById(id)){
+            repo.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+    @Transactional
+    public Produto atualizar(Long id, Produto atual){
+        if (repo.existsById(id)){
+            atual.setId(id);
+            return repo.save(atual); // achou
+        }
+        return null; // não achou para atualizar
     }
 }
